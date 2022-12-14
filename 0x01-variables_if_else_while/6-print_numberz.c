ad22c2edcbbe8018386e7d91e25e2888ca3c7ba8
Beautyzita <oyikobherself@gmail.com>
2022-12-13 16:01:45 -0800

    6 printss

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c = '0';
+
+
+	if (c <= '9')
+{
+	putchar(c);
+	c++;
+}
+putchar('\n');
+return (0);
+}
