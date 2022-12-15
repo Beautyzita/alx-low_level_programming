d70fd7f2c306ec5bbf322a1a783fc95a9aa05498
Beautyzita <oyikobherself@gmail.com>
2022-12-15 12:06:03 -0800

    isomer

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - this is to check for lowercase character
+ *@c: is the int to be checked
+ * Return: 1 if the letter is lowercase, 0 otherwise
+ */
+int _islower(int c)
+{
+	if (c > 0)
+{
+	return (1);
+	else
+	return (0);
+}
+}
