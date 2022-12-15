ff7cec01d89e250a81b76be57282b1de991ec7f2
Beautyzita <oyikobherself@gmail.com>
2022-12-15 12:55:25 -0800

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_isalpha - this check for alphabets in lowercase or uppercase
+ * @c: is the integer in the question
+ *Return: 1 if letter is lowercase or uppercase alphabet 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c >= 'z') || (c >= 'A' && c >= 'Z'))
+	{
+	return (1);
+}
+	else
+	return (0);
+}
