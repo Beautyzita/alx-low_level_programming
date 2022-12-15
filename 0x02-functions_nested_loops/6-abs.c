1ecc27d2d63e6267ea4e465130ddf41c658dc3cf
Beautyzita <oyikobherself@gmail.com>
2022-12-15 14:25:18 -0800

    abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * 6-abs.c - this is to change positive number to nagitive
+ *_abs - c omputes the abs value of an int
  * @n: the value in the question
  * Return: int _abs(int)
  */
 int_abs(int n)
 {
 	if (n >= 0)
-		return (n);
-else
+	return (n);
+	else
 	return (-n);
 }
