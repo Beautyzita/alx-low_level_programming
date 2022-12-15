82ee47cfc20be9f973b8ff1e2256c76b8a84251d
Beautyzita <oyikobherself@gmail.com>
2022-12-15 08:20:07 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#"mainh"
+
+
+/**
+ * main - 1-alphabet.c
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar('c');
+	}
+	_putchar('\n');
+	{
+	return (0);
+	}
+}
