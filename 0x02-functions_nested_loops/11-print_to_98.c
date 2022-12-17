97dfe3d188fba9951e43a3287a7287a098eadb94
Beautyzita <oyikobherself@gmail.com>
2022-12-17 07:29:19 -0800

    print

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 # include "main.h"
+
 /**
  * print_to_98 - print all numbers from input to 98
  * @n: the starting number
@@ -16,4 +17,5 @@ void print_to_98(int n)
 	while (n > 98)
 	printf("%d, ", n++);
 	printf("%d\n", n);
+	}
 }
