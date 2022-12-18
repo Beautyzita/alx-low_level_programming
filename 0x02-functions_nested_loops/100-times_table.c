06bbce8c3146a869f3afd415388ad4a58308659d
Beautyzita <oyikobherself@gmail.com>
2022-12-18 08:50:43 -0800

    natural numbers

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,7 +2,7 @@
 
 /**
  * print_times_table - prints times table
- * @n : times table to use
+ * @n: times table to use
  * Return: void
  */
 void print_times_table(int n)
---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - This is to find multiples of 3 and 5 that are below 1024
+ * @i: the integer in the question
+ * Return: return the value of the integer
+ */
+int main(int i)
+{
+	for (i = 0; i <= 1024; i++)
+	{
+		if (i % 15 == 0)
+		{
+			printf("%d\n" i);
+		}
+		else if (i % 5 == 0)
+		{
+			printf("%d\n" i);
+		}
+		else if (i % 3 == 0)
+		{
+			printf("%d\n" i);
+		}
+		printf("%d\n" i);
+	}
+}
