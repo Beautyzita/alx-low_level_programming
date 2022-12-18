c54b8272d93e2f7edd3b2ad1312049df7748a696
Beautyzita <oyikobherself@gmail.com>
2022-12-18 09:44:47 -0800

    natural numbers

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,25 +3,28 @@
 
 /**
  * main - This is to find multiples of 3 and 5 that are below 1024
- * @i: the integer in the question
  * Return: return the value of the integer
  */
-int main(int i)
+int main(void)
+
 {
-	for (i = 0; i <= 1024; i++)
-	{
-		if (i % 15 == 0)
-		{
-			printf("%d\n", i);
-		}
-		else if (i % 5 == 0)
-		{
-			printf("%d\n", i);
-		}
-		else if (i % 3 == 0)
-		{
-			printf("%d\n", i);
-		}
-		printf("%d\n", i);
-	}
+int i;
+int sum = 0;
+
+for (i = 0; i <= 1024; i++)
+{
+if (i % 5 == 0)
+{
+printf("%d\n", i);
+}
+if (i % 3 == 0)
+{
+printf("%d\n", i);
+}
+{
+sum = 0 + 1;
+}
+printf("%d\n", sum);
+}
+return (0);
 }
