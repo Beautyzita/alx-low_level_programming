55395ece3603f55f10df3645fec4d40c9480ef96
Beautyzita <oyikobherself@gmail.com>
2022-12-20 08:35:38 -0800

    puts

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - print every other character of a string
+ * @str: the string to be treated
+ * Return: this return void
+ */
+void puts2(char *str)
+{
+	int i;
+
+	int = 0;
+
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+	for (i = 0; i < i += 2)
+	putchar(str[i]);
+
+}
+	putchar('\n');
+}
