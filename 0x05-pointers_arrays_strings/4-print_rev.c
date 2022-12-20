d5d66ee3a485eaa13d74705bc3f9c0a9e931382c
Beautyzita <oyikobherself@gmail.com>
2022-12-20 07:56:09 -0800

    print

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio>
+
+/**
+* print_rev - it prints string in the revise order
+* @s: it is the string to be printed
+* Return: it is to return the sring
+*/
+void print_rev(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+	for (c -= 1; c >= 0; c--)
+	{
+		_putchar(s[c]);
+	}
+		_putchar('\n');
+}
