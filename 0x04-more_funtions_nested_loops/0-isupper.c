15a068422d83ffef23c34d9f3d9fd9bbfaa24dee
Beautyzita <oyikobherself@gmail.com>
2022-12-21 07:13:31 -0800

    isupper

--- 0x04-more_funtions_nested_loops/0-isupper.c
+++ 0x04-more_funtions_nested_loops/0-isupper.c
@@ -3,7 +3,7 @@
 
 /**
  * _isupper - this checks for upper character
- * @c: this is the character to be checked
+ * @c: This is the character to be checked
  * Return: 1 if c is an uppercase. 0 otherwise
  */
 int _isupper(int c)
