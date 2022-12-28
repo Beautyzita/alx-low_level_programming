45e72d228c6421e0d08d855a2622b3eb131cba7b
Beautyzita <oyikobherself@gmail.com>
2022-12-28 07:06:53 -0800

    supper

--- 0x04-more_funtions_nested_loops/0-isupper.c
+++ 0x04-more_funtions_nested_loops/0-isupper.c
@@ -7,6 +7,13 @@
  * Return: 1 if c is an uppercase. 0 otherwise
  */
 int _isupper(int c)
+}
+if (c >= 'A' && C <= 'Z')
 {
-	return (c >= 'A' && c <= 'Z');
+	return (1);
+}
+else
+{
+	return (0);
+}
 }
