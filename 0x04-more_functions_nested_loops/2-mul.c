cdfe761f59614e7810d639560c9bb0e387fb4f43
Beautyzita <oyikobherself@gmail.com>
2022-12-28 08:07:17 -0800

    mul

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,11 +3,12 @@
 /**
  * mul - multiplication for two integer
  * @a: the first integer to be used in the multiplication
- * @b: the second integer in the question
+ * @b: the second integer in the questioi
  * Return: this return the result of the multiplication
  */
 int mul(int a, int b)
 {
-	c = a * b;
+	int c = a * b;
+	i
 	return (c);
 }
