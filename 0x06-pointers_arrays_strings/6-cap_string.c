0d789f135cac01795f32c7247854f81123a0dfed
Beautyzita <oyikobherself@gmail.com>
2022-12-29 06:52:29 -0800

    sting

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,11 +9,11 @@ char *cap_string(char *str)
 {
 	int index = 0;
 
-	while (str[inde])
+	while (str[index])
 	{
-	while (i(str[index] >= 'a' && str[index] <= 'Z'))
+	while (*(str[index] >= 'a' && str[index] <= 'Z'))
 	index++;
-	if (str[index - 1] == ' '  ||
+	if (str[index - 1] == ' ' ||
 	str[index - 1] == '\t' ||
 	str[index - 1] == '\n' ||
 	str[index - 1] == ',' ||
