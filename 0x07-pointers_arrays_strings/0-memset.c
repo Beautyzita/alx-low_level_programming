2a2a9846751f494538b6c95169c1b4965913c7d7
Beautyzita <oyikobherself@gmail.com>
2023-01-01 10:08:57 -0800

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,10 +6,7 @@
 * @s: A pointer to the memory area to be filled
 * @b: The character to be fill the memory area with
 * @n: The number of bytes to be filled
-* describtion _memset: over there
-*
 * Return: A pointer to the filled memory area @s
-*
 */
 char *_memset(char *s, char b, unsigned int n)
 {
