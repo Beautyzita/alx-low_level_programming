2b1b228225c0be75339f79723ddeaa44d4a6bde8
Beautyzita <oyikobherself@gmail.com>
2023-01-01 12:59:13 -0800

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strstr - finds the first occurance of the substring
+ * needle in the string haystack
+ * @haystack: entire string
+ * @needle: substring
+ * Return: pointer to the beginning of located subsring or
+ * null if the subsring is not found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *bhaystack;
+	char *pneedle;
+
+	while (*haystack != '\0')
+	{
+		bhatstack = haystack;
+		pneedle = needle;
+	while (*haystack != '\0' && *pneedle != '\0' && *haystack == *pneedle)
+	{
+		haystack++;
+		pneedle++;
+	}
+	if (!*pneedle)
+		return (bhaystack);
+	haystack = bhaystack + 1;
+	}
+	return (0);
+}
