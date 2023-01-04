6ed6dbb6168ad52a7fe3bd0b6f1c47cf0bf59fa9
Beautyzita <oyikobherself@gmail.com>
2023-01-04 06:14:16 -0800

    strlen

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "maim.h"
+#include <stdio.h>
+
+/**
+*_strlen_recursion - print the length of a string
+* @s: the string length to be printed
+* Return: no return
+*/
+int _strlen_recursion(char *s)
+{
+if (*s)
+{
+_print _strlen_recursion(s + 1);
+_putchar(*s);
+}
+}
+
+
