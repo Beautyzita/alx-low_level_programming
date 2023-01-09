a2852af7f67349abd80a0440d1d9f2da82390f46
Beautyzita <oyikobherself@gmail.com>
2023-01-09 07:36:48 -0800

    args

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*main - Prints the name of the program
+*@argc: Number of arguments
+*@argv: Array of arguments
+*Return: Always 0 (Success)
+*/
+int (void)(char argc _ atribute_(unused), char *argv[])
+{
+
+	printf("%s\n", *argv);
+	return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main -  prints the number of arguments passed in the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc; /*Ignore argv*/
+	printf("%d\n", argc - 1);
+	return (0);
+}
