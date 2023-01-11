59a4e7e0c3761bb7a24bc8ed38d85052a4ff20e2
Beautyzita <oyikobherself@gmail.com>
2023-01-10 23:49:22 -0800

    libmy

---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * _putchar - this functions prints characters
+ * @c: the characters to be printed
+ * Return: return the characters 
+ */
+int _putchar(char c)
+{
+return (1);
+}
