1d1b9454c94c7a814993804f1f3ea960aaf44e39
Beautyzita <oyikobherself@gmail.com>
2023-01-26 03:40:48 -0800

    strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -25,5 +25,9 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("(nil)");
 		else
 			printf("%s", str);
+		if (index != (n - 1) && separator != NULL)
+			printf("%s", separator);
 	}
+	printf("\n");
+	va_end(strings);
 }
