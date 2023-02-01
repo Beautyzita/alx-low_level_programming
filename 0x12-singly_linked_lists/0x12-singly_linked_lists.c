d669c0783cfc97546140a8fecc335838ab8b505e
Beautyzita <oyikobherself@gmail.com>
2023-02-01 06:34:00 -0800

    singly

---
+++ 0x12-singly_linked_lists/0x12-singly_linked_lists.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void __attribute__((constructor)) hare(void);
+/**
+ * hare - prints a string before the main function in execution
+ */
+	void hare(void)
+{
+	printf("You're beat! and yet, you must allow,"
+		"\nI bore my house upon my back!\n");
+}
