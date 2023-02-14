e265b9a475ae3032eba52f7ca61e01d7e8ac44e6
Beautyzita <oyikobherself@gmail.com>
2023-02-14 13:35:20 -0800

    binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -3,7 +3,7 @@
  * print_binary - equivalent of a decimal number
  * @n: number to print in binary
  */
-void print_binary(unsigned long int n);
+void print_binary(unsigned long int n)
 {
 	int i, count = 0;
 	unsigned long int current;
@@ -22,5 +22,3 @@ for (i = 63; i >= 0; i--)
 if (!count)
 _putchar('0');
 }
-
-
